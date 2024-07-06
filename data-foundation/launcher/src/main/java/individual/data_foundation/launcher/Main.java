4d43ad47414d52db4bdab4b5766f2a2e0054e6e4
huangww01 <huangww01@mingyuanyun.com>
2024-07-06 17:59:35 +0800

    添加程序入口

--- data-foundation/launcher/src/main/java/individual/data_foundation/launcher/Main.java
+++ data-foundation/launcher/src/main/java/individual/data_foundation/launcher/Main.java
@@ -3,6 +3,7 @@ package individual.data_foundation.launcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Same as @SpringBootConfiguration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
