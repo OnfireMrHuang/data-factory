3195f300eaab4d081437f4267d30671f9fe49717
huangww01 <huangww01@mingyuanyun.com>
2024-07-26 14:05:30 +0800

    debug

--- data-foundation/web/src/main/java/individual/data_foundation/web/controller/monitor/Environment.java
+++ data-foundation/web/src/main/java/individual/data_foundation/web/controller/monitor/Environment.java
@@ -2,12 +2,15 @@ package individual.data_foundation.web.controller.monitor;
 
 import individual.data_foundation.common.dto.Response;
 import individual.data_foundation.common.models.monitor.EnvironmentInfo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/monitor/environment")
 public class Environment {
+
+    @GetMapping("/info")
     public Response<EnvironmentInfo> GetEnvironmentInfo() {
         EnvironmentInfo environmentInfo = EnvironmentInfo.builder().javaVersion("jdk-13").build();
         return Response.success(environmentInfo);
