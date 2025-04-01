2f66e3af511c29c3d7f3adf69bbedfcf151bf0c6
huangww01 <huangww01@mingyuanyun.com>
2025-04-01 11:34:29 +0800

    调低jDK版本为13

--- data-foundation/data-foundation-launcher/src/main/java/io/github/hww/data/foundation/Main.java
+++ data-foundation/data-foundation-launcher/src/main/java/io/github/hww/data/foundation/Main.java
@@ -1,13 +1,11 @@
 package io.github.hww.data.foundation;
 
 
-import lombok.extern.slf4j.Slf4j;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 @MapperScan("io.github.hww.data.foundation.dao.mapper") // 指定mapper所在的包
 public class Main {
