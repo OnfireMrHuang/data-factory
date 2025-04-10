d69e801111837b163174aad74b65cc90188681c9
huangww01 <huangww01@mingyuanyun.com>
2025-04-10 15:13:23 +0800

    添加dataX组件

---
+++ data-foundation/data-foundation-common/src/main/java/io/github/hww/data/foundation/common/component/datax/DatXContainer.java
@@ -0,0 +1,23 @@
+package io.github.hww.data.foundation.common.component.datax;
+
+import io.github.hww.data.foundation.common.component.datax.utilities.ConfigParser;
+import lombok.extern.slf4j.Slf4j;
+import com.alibaba.datax.common.util.Configuration;
+import com.alibaba.datax.core.Engine;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Supplier;
+
+@Slf4j
+@Component
+public class DatXContainer {
+
+    public void start(Supplier<String> jobContent) {
+
+        Configuration configuration = ConfigParser.parse(jobContent);
+        log.info("DataXEngin启动job参数:\n{}", configuration);
+
+        Engine engine = new Engine();
+        engine.start(configuration);
+    }
+}
